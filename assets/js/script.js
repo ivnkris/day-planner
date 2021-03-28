b7598420f13af5c97b3d0e3d7dbc28f844a19452
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-28 12:38:44 +0100

    added boilerplate functions for memory tasks and event listener to invoke getTasksFromMemory function on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,16 @@ const setTextAreaColours = () => {
   allTextArea.each(setColour);
 };
 
+const setTasksIntoMemory = () => {};
+
+const getTasksFromMemory = () => {
+  console.log("ready");
+};
+
 // event listeners to invoke setCurrentTime() and setTextAreaColours() when DOM has rendered
 $(document).ready(setCurrentTime);
 $(document).ready(setTextAreaColours);
+$(document).ready(getTasksFromMemory);
 
 // function to check the time every 10 seconds and reset textarea colours if needed
 setInterval(setTextAreaColours, 10000);
