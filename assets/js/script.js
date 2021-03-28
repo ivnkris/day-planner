5c539c283616c2eba6e1218e91be79decf3357ce
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-28 12:21:35 +0100

    added textarea colours (bugs - WIP)

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,18 @@ const setTextAreaColours = () => {
   const currentTime = moment().format("H");
   const divContainer = $(".container");
   const allTextArea = divContainer.children().children("textarea");
-  const setColour = () => {
-    console.log($(this).data(time));
+  const setColour = (index) => {
+    const thisTime = allTextArea[index].dataset.time;
+    console.log(thisTime);
+    console.log(currentTime);
+    console.log(index);
+    if (thisTime < currentTime) {
+      $(allTextArea[index]).addClass("past");
+    } else if (thisTime === currentTime) {
+      $(allTextArea[index]).addClass("present");
+    } else if (thisTime > currentTime) {
+      $(allTextArea[index]).addClass("future");
+    }
   };
   allTextArea.each(setColour);
 };
