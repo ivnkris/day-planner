b3f19ac959fa3f2494e4be132ca1fbc355a86474
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-28 11:35:31 +0100

    added function to set colour of each text area depending on current time (bugs - WIP)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,15 @@ const setCurrentTime = () => {
   $("#currentDay").text(currentTime);
 };
 
+const setTextAreaColours = () => {
+  const currentTime = moment().format("H");
+  const divContainer = $(".container");
+  const allTextArea = divContainer.children().children("textarea");
+  const setColour = () => {
+    console.log($(this).data(time));
+  };
+  allTextArea.each(setColour);
+};
+
 $(document).ready(setCurrentTime);
+$(document).ready(setTextAreaColours);
